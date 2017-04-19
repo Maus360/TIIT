c7779c88c02bfd2bf0838712434a38a131ccc688
Maks <maksimochka200@gmail.com>
2017-04-19 15:29:40 +0300

    Принята 1-ая лабораторная

--- 1laba/funcs.py
+++ 1laba/funcs.py
@@ -1,55 +1,59 @@
-def b(arr, v, tl, tr):
-    if (tl == tr):
-        laba[v] = arr[tl]
-    else:
 
-        tm = (tl + tr) // 2
-        b(arr, v * 2, tl, tm)
-        b(arr, v * 2 + 1, tm + 1, tr)
-        laba[v] = laba[v * 2] + laba[v * 2 + 1]
-
-
-k = 0
-
-
-def build(arr):
-    global laba
-    global k
-    k=0
-    laba = [0] * len(arr) * 4
-    b(arr, 1, 0, len(arr) - 1)
-    print(laba)
-    return laba
-
-
-def u(v, tl, tr, pos, add):
-    if tl == tr:
-        laba[v] += add
-    else:
-        tm = (tl + tr) // 2
-        if pos <= tm:
-            u(v * 2, tl, tm, pos, add)
+class tree():
+    def __init__(self, arr):
+        self.laba = [0]*4*len(arr)
+        self.arr = arr
+    def b(self,arr, v, tl, tr):
+        if (tl == tr):
+            self.laba[v] = self.arr[tl]
         else:
-            u(v * 2 + 1, tm + 1, tr, pos, add)
-        laba[v] = laba[v * 2] + laba[v * 2 + 1]
+
+            tm = (tl + tr) // 2
+            self.b(arr, v * 2, tl, tm)
+            self.b(arr, v * 2 + 1, tm + 1, tr)
+            self.laba[v] = self.laba[v * 2] + self.laba[v * 2 + 1]
 
 
-def update(arr, l, r, x):
-    if l >= 0 and r <= len(arr):
-        for i in range(l - 1, r):
-            u(1, 0, len(arr) - 1, i, x)
-        return laba
-    else:
-        Exception("ERROR")
+    def build(self):
+
+        laba = [0] * len(self.arr) * 4
+        self.b(self.arr, 1, 0, len(self.arr) - 1)
+        print(self.laba)
 
 
-def get(v, tl, tr, key):
-    global k
-    if tr == tl:
-        if laba[v] == key:
-            k += 1
-        return
-    tm = (tl + tr) // 2
-    get(v * 2, tl, tm, key)
-    get(v * 2 + 1, tm + 1, tr, key)
-    return k
+    def u(self, v, tl, tr, pos, add):
+        if tl == tr:
+            self.laba[v] += add
+        else:
+            tm = (tl + tr) // 2
+            if pos <= tm:
+                self.u(v * 2, tl, tm, pos, add)
+            else:
+                self.u(v * 2 + 1, tm + 1, tr, pos, add)
+            self.laba[v] = self.laba[v * 2] + self.laba[v * 2 + 1]
+
+
+    def update(self, l, r, x):
+
+        if l >= 0 and r <= len(self.arr):
+            for i in range(l - 1, r):
+                self.u(1, 0, len(self.arr) - 1, i, x)
+            return self.laba
+        else:
+            Exception("ERROR")
+
+
+    def get(self, key):
+        self.k = 0
+        return self.g(1, 0, len(self.arr)-1, key)
+
+
+    def g(self, v, tl, tr, key):
+        if tr == tl:
+            if self.laba[v] == key:
+                self.k += 1
+            return
+        tm = (tl + tr) // 2
+        self.g(v * 2, tl, tm, key)
+        self.g(v * 2 + 1, tm + 1, tr, key)
+        return self.k
--- 1laba/tiit.py
+++ 1laba/tiit.py
@@ -1,17 +1,17 @@
-from funcs import get, build, update
+from funcs import tree
 from tests import test
 
 
 def main(k=0):
     for t in enumerate(test):
         print("====================== Test", str(t[0]+1),"================================================")
-        arr = t[1]["arr"]
+        Maks = tree(t[1]["arr"])
         start1, end1, value1 = t[1]["update"]
         value2 = t[1]["search"]
-        build(arr)
-        print("Количество элементов, равных", value2,"\n", get(1, 0, len(arr) - 1, value2))
+        Maks.build()
+        print("Количество элементов, равных", value2,"\n", Maks.get(value2))
         print("Каждый элемент, начиная с",start1," и заканчивая",end1,"изменили на", value1,
-              "\n", update(arr, start1, end1, value1))
+              "\n", Maks.update(start1, end1, value1))
 
 
 main()
